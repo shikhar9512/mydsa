5b774f249811a73778d9a49a917779b5f6158d58
shikhar9512 <131337967+shikhar9512@users.noreply.github.com>
2023-06-04 21:50:30 +0530

    Add files via upload

---
+++ recursion1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    //base  case
+    if (n==0)
+     return 1;
+
+    int choti=factorial(n-1);
+    int badi=n*choti;
+    return badi; 
+}
+
+
+int main(){
+    
+    int n;
+    cin>>n;
+
+    int ans = factorial(n);
+    cout<<"The required factorial is"<<ans<<endl;
+      
+    return 0;
+}
